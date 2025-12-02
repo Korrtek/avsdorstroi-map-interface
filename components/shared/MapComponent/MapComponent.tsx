6f232ffc1cda5a5ff28f82234474c6c752b81a43
Bondrewd <beluaev08@gmail.com>
2025-12-02 13:59:22 +0500

    -Установил библиотеку leaflet -Создал компонент MapComponent в котором будет находиться карта -Написал логику карты -Компонент карты будет создаваться внутри компонента MarkForm -Клик по карте и ширина/долгота будут передаваться как пропс в MapComponent

---
+++ components/shared/MapComponent/MapComponent.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from 'react'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import styles from './MapComponent.module.scss'
+
+//  Пути к иконкам через опции иконки взяты с CDN самой библы
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
+interface MapComponentProps {
+  markerPosition: [number, number]
+
+  zoom?: number
+  className?: string
+}
+
+export const MapComponent: React.FC<MapComponentProps> = ({
+  markerPosition,
+  
+  zoom = 13,
+}) =>  {
+    // ссылки на Dom элементы
+  const mapRef = useRef<L.Map | null>(null)
+  const markerRef = useRef<L.Marker | null>(null)
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    // Создание карты при монтировании компонента
+    if (!mapContainerRef.current || mapRef.current) return
+    mapRef.current = L.map(mapContainerRef.current).setView(
+      markerPosition,
+      zoom,
+    )
+    // Использую стандартную карту
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(mapRef.current)
+    
+    markerRef.current = L.marker(markerPosition)
+      .addTo(mapRef.current)
+      
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove()
+        mapRef.current = null
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (markerRef.current) {
+      markerRef.current.setLatLng(markerPosition)
+    }
+
+    if (mapRef.current) {
+      mapRef.current.setView(markerPosition)
+    }
+  }, [markerPosition])
+
+  return <div ref={mapContainerRef} className={styles.map} />
+}
--- components/shared/MarkForm/MarkForm.tsx
+++ components/shared/MarkForm/MarkForm.tsx
@@ -1,46 +1,68 @@
-import style from './MarkForm.module.scss'
+'use client'
+import styles from './MarkForm.module.scss'
 import { Container } from '@/components/ui/Container/Container';
 import { Input } from '@/components/ui/Input/Input';
+import { MapComponent } from '../MapComponent/MapComponent';
+import { useState } from 'react';
 interface Props {
     className?: string;
 }
-
+// Форма, в которую будут заноситься данные для метки, при внесении данных в поля с широтой и долготой будет происходить взаимодейсткие с картой
 export const MarkForm: React.FC<Props> = ( { className }) => {
+  
+  const [markerPosition, setMarkerPosition] = useState<[number, number]>([58.002407, 58.002407])
+
+
   return (
     <Container>
-    <div className={style.markForm}>
-            <Input
-            label="Название"
-            placeholder="Курьер №1"
-            />
-            <Input
-            label="Широта"
-            placeholder="широта"
-            />
-            <Input
-            label="Долгота"
-            placeholder="Долгота"
-            />
-            <Input
-            label="Email"
-            placeholder="mail@example.com"
-            type="email"
-            />
-            <Input
-            label="Телефон"
-            placeholder="Телефон +7(800)5553535"
-            type="tel"
-            />
-            <Input
-            label="Часы работы"
-            placeholder="Пн-пт, 9:00 - 19:00"
-            />
-            <Input
-            label="Название Компании"
-            placeholder="ООО Название компании"
-            />
-           
+      <div className={styles.markForm}>
+        <div className={styles.markFormWrapper}>
+          <div className={styles.markColumn} >
+
+          
+              <Input
+              label="Название"
+              placeholder="Курьер №1"
+              />
+              <Input
+              label="Широта"
+              placeholder="широта"
+              />
+              <Input
+              label="Долгота"
+              placeholder="Долгота"
+              />
+              <Input
+              label="Email"
+              placeholder="mail@example.com"
+              type="email"
+              />
+              <Input
+              label="Телефон"
+              placeholder="Телефон +7(800)5553535"
+              type="tel"
+              />
+              <Input
+              label="Часы работы"
+              placeholder="Пн-пт, 9:00 - 19:00"
+              />
+              <Input
+              label="Название Компании"
+              placeholder="ООО Название компании"
+              />
+        </div>
+        <div className={styles.mapWrapper}>
+        <div className={styles.mapContainer}>
+
+          
+        <MapComponent
+        markerPosition={markerPosition}
+        
+        />
+        </div>
+        </div>
+      </div>
     </div>
-    </Container>
+  </Container>
   );
 };

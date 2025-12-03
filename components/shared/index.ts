82e86b3b2fb5800650508f63b0d06f87b8f916b6
Bondrewd <beluaev08@gmail.com>
2025-12-03 23:27:43 +0500

    - Исправил верстку в компоненте input

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,7 +1,6 @@
 export { ClearMarksButton } from './ClearMarksButton/ClearMarksButton';
 export { CreateMarkButton } from './CreateMarkButton/CreateMarkButton';
 export { Header } from './Header/Header';
-export { MapComponent } from './MapComponent/MapComponent';
 export { MarkForm } from '@/components/shared/MarkForm/MarkForm';
 export { MarkManager } from './MarkManager/MarkManager';
 export { MarkPreset } from './MarkPreset/MarkPreset';
